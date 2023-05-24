76ba31c18c73a52d0ba4c62a356bdc4d3aa989a4
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-24 08:28:24 -0400

    three-project-01 | class Camera

--- Experience/Experience.js
+++ Experience/Experience.js
@@ -1,4 +1,6 @@
 import * as THREE from 'three';
+import Size from './Utils/Sizes.js';
+import Camera from './Utils/Camera.js';
 
 export default class Experience{
     static instance;
@@ -7,6 +9,10 @@ export default class Experience{
             return Experience.instance
         }
         Experience.instance = this;
-        this.canvas = canvas
+        this.canvas = canvas;
+        this.scene = new THREE.Scene();
+        this.sizes = new Size();
+        this.camera = new Camera();
+        console.log(this.camera, this.sizes, this.scene, this.canvas);
     }
 }
--- Experience/Utils/Sizes.js
+++ Experience/Utils/Sizes.js
@@ -1,11 +1,11 @@
 export default class Sizes{
     constructor(){
-        this.width = window.innerWidth;
+        this.width = window.innerWidth; // <--- estas 3 estan en la documentacion de parametros de three
         this.height = window.innerHeight;
         this.aspect = this.width/this.height;
-        this.pixelRatio = Math.min(window.devicePixelRatio, 2)
+        this.pixelRatio = Math.min(window.devicePixelRatio, 2) // <--- documentation
 
-        window.addEventListener('resize', () => {
+        window.addEventListener('resize', () => { // <--- escuchar el tamagnio de la ventana
             this.width = window.innerWidth;
             this.height = window.innerHeight;
             this.aspect = this.width/this.height;
