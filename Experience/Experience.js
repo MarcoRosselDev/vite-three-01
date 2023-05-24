83a4b5633c0c46b87f3fec6239cfeba082233e1e
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-24 11:35:51 -0400

    three-project-01 | resize emitEvents

--- Experience/Experience.js
+++ Experience/Experience.js
@@ -17,5 +17,21 @@ export default class Experience{
         this.sizes = new Size();
         this.camera = new Camera();
         this.renderer = new Renderer();
+
+        this.sizes.on('resize', ()=>{
+            this.resize();
+        })
+
+        this.time.on('update', ()=>{
+            this.update();
+        })
+    }
+    resize(){
+        this.camera.resize();
+        this.renderer.resize();
+    }
+    update(){
+        this.camera.update();
+        this.renderer.update();
     }
 }
