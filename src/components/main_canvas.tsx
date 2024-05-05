eda117cf464718d8ebcaeb38a633c46a0936051b
Marco Rossel <andresmarcorossel@gmail.com>
2024-05-05 00:01:10 -0400

    ts three 00 | 07

--- src/components/main_canvas.tsx
+++ src/components/main_canvas.tsx
@@ -1,6 +1,4 @@
 import { Canvas } from "@react-three/fiber";
-import {Box} from './Box'
-import { App } from "./Magic_box";
 import { OrbitControls } from "@react-three/drei";
 import { Box_mesh } from "./Box_mesh";
 
@@ -10,10 +8,8 @@ export function Main_canvas(){
       <ambientLight intensity={Math.PI / 2} />
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
       <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-      {/* <Box /> */}
       <Box_mesh />
       <OrbitControls />
-      {/* <App key={1} position={[1, 1, 1]}/> */}
     </Canvas>
   )
 }
