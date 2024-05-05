1e6068d0a9f8100153631566379fb9eb404d487a
Marco Rossel <andresmarcorossel@gmail.com>
2024-05-04 23:02:10 -0400

    ts three 00 | 04

---
+++ src/components/Side.tsx
@@ -0,0 +1,27 @@
+import { Environment, MeshPortalMaterial, useGLTF } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+
+export function Side({ rotation = [0, 0, 0], bg = '#ffffff', children, index }:{rotation :Array<number>, bg:string, children:any, index: number}) {
+  const mesh = useRef()
+  const { nodes } = useGLTF('./model/aobox-transformed.glb')
+  useFrame((state, delta) => {
+    mesh.current.rotation.x = mesh.current.rotation.y += delta
+  })
+
+  return (
+    <MeshPortalMaterial attach={`material-${index}`}>
+      <ambientLight intensity={0.5} />
+      <Environment preset="dawn" />
+      <mesh castShadow receiveShadow rotation={rotation} geometry={nodes.Cube.geometry}>
+        <meshStandardMaterial aoMapIntensity={1} aoMap={nodes.Cube.material.aoMap} color={bg} />
+        <spotLight castShadow color={bg} intensity={8} position={[3, 3, 3]} angle={0.35} penumbra={0.8} shadow-normalBias={0.1} shadow-bias={0.00001} />
+      </mesh>
+      {/** The shape */}
+      <mesh castShadow receiveShadow ref={mesh}>
+        {children}
+        <meshLambertMaterial color={bg} />
+      </mesh>
+    </MeshPortalMaterial>
+  )
+}
--- src/components/main_canvas.tsx
+++ src/components/main_canvas.tsx
@@ -2,14 +2,16 @@ import { Canvas } from "@react-three/fiber";
 import {Box} from './Box'
 import { App } from "./Magic_box";
 import { OrbitControls } from "@react-three/drei";
+import { Box_mesh } from "./Box_mesh";
 
 export function Main_canvas(){
   return (
-    <Canvas >
+    <Canvas shadows camera={{ position: [-3, 0.5, 3] }}>
       <ambientLight intensity={Math.PI / 2} />
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
       <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-      <Box />
+      {/* <Box /> */}
+      <Box_mesh />
       <OrbitControls />
       {/* <App key={1} position={[1, 1, 1]}/> */}
     </Canvas>
